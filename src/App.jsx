9112bddaf3cc6fe36bb67f4c6db6eb4659d5e32e
shahbaz141 <shahbazhussain141044@gmail.com>
2023-12-29 17:05:16 +0500

    reducer functions and dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import Header from './components/Header'
 import Loader from './components/Loader';
 import Main from './components/Main';
+import Error from './components/Error';
+import Start from './components/Start';
 const initialState={
   status:"loading",
   questions:[],
 }
 const reducer=(state,action)=>{
-
+  if(action.type==="dataReceived")
+  return{
+...state,
+status:"ready",
+questions:action.payload}
+if (action.type==="dataFailed")
+return{
+  ...state,
+  status:"error",
 }
+}
+
 const App = () => {
   const [{status},dispatch]=useReducer(reducer,initialState);
+  useEffect(()=>{
+    const getQuestions=async()=>{
+      try{
+        const res =await fetch("http://localhost:3004/questions")
+      const data =await res.json();
+      dispatch({type:"dataReceived",payload:data});
+      }
+      catch{
+        dispatch({type:"dataFailed"});
+      }
+      
+    }
+    getQuestions();
+  },[])
   return (
     <div className='app'>
       <Header/>
       <Main>
+        {status==="ready"&&<Start/>}
         {status==="loading"&&<Loader/>}
+        {status==="error"&&<Error/>}
       </Main>
     </div>
   )
---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Error = () => {
+  return (<div>
+    <p className='error'><h1>i am an error</h1></p>
+    </div>
+  )
+}
+
+export default Error
